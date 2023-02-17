d362a07dcf36aef45884f621e3a6b77897b15009
Neo-Stone <40294190+Neo-Stone@users.noreply.github.com>
2023-02-17 17:33:14 +0100

    cdrom patch

--- yabause/src/cdbase.h
+++ yabause/src/cdbase.h
@@ -18,25 +18,6 @@
     along with Yabause; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
-/*
-        Copyright 2019 devMiyax(smiyaxdev@gmail.com)
-
-This file is part of YabaSanshiro.
-
-        YabaSanshiro is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-YabaSanshiro is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-along with YabaSanshiro; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
 
 /*! \file cdbase.c
     \brief Header for Dummy and ISO, BIN/CUE, MDS CD Interfaces
@@ -60,12 +41,27 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 typedef struct
 {
+   u8 ctrladr;
+   u8 tno;
+   u8 point;
+   u8 min;
+   u8 sec;
+   u8 frame;
+   u8 zero;
+   u8 pmin;
+   u8 psec;
+   u8 pframe;
+} CDInterfaceToc10;
+
+typedef struct
+{
         int id;
         const char *Name;
         int (*Init)(const char *);
         void (*DeInit)(void);
         int (*GetStatus)(void);
         s32 (*ReadTOC)(u32 *TOC);
+        s32 (*ReadTOC10)(CDInterfaceToc10 *TOC);
         int (*ReadSectorFAD)(u32 FAD, void *buffer);
         void (*ReadAheadFAD)(u32 FAD);
 		void(*SetStatus)(int status);
